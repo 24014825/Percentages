05a822995ae894029ec299828dd9cb017f646b17
24014825 <24014825@mvula.univen.ac.za.>
2026-08-20 18:07:23 +0200

    First commit

--- Percentages/src/main/java/com/mycompany/percentages/Percentages.java
+++ Percentages/src/main/java/com/mycompany/percentages/Percentages.java
@@ -9,16 +9,22 @@ package com.mycompany.percentages;
  * @author 24014825
  */
 public class Percentages {
+    
+    
     public static void computePercent(double num1, double num2) {
         double percent = (num1 / num2) * 100;
         System.out.println(num1 + " is " + percent + " percent of " + num2);
     }
 
     public static void main(String[] args) {
-        double firstNumber = 2.0;
-        double secondNumber = 5.0;
+        double a = 2.0;
+        double b = 5.0;
 
-        computePercent(firstNumber, secondNumber);
-          computePercent(secondNumber, firstNumber);
-    }
+        
+        computePercent(a, b);
+        
+       
+        computePercent(b, a);
+    }  
+    
 }
---
+++ Percentages2/src/main/java/com/mycompany/percentages2/Percentages2.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.percentages2;
+
+/**
+ *
+ * @author 24014825
+ */
+import java.util.Scanner;
+
+public class Percentages2 {
+    
+   
+    public static void computePercent(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Cannot divide by zero!");
+            return;
+        }
+        double percent = (num1 / num2) * 100;
+        System.out.println(num1 + " is " + percent + " percent of " + num2);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = input.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double b = input.nextDouble();
+
+      
+        computePercent(a, b);
+        
+      
+        computePercent(b, a);
+
+        input.close();
+    }
+}
+    
+
